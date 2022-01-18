11c5ddfa3572fda31328e1785311c314eb83fd32
root <root@80-78-240-68.cloudvps.regruhosting.ru>
2022-01-18 12:58:56 +0300

    fixes

--- src/resources/views/site.blade.php
+++ src/resources/views/site.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Laravel') }}</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
     </head>
     <body>
         <div id="root"></div>
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="/js/app.js" defer></script>
     </body>
 </html>
